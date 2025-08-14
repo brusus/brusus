08213e2da020901828e6af18c6f97253691d7480
DieGon7771 <bruttodiego6@gmail.com>
2025-08-14 20:51:54 +0200

    Update build.gradle.kts

--- StreamingCommunity/build.gradle.kts
+++ StreamingCommunity/build.gradle.kts
@@ -1,12 +1,12 @@
 // use an integer for version numbers
-version = 20
+version = 21
 
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "TV Shows and Movies from StreamingCommunity (now StreamingUnity)"
-    authors = listOf("doGior")
+    description = "Film e SerieTV da StreamingCommunity"
+    authors = listOf("doGior","DieGon")
 
     /**
     * Status int as the following:
