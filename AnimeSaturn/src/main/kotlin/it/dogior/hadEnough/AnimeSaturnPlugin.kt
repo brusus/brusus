a7276feeafccec2f00ce3284e50386544534ad3c
brusus <rafael.fiallo93@gmail.com>
2025-08-27 07:41:35 +0200

    Update AnimeSaturnPlugin.kt

--- AnimeSaturn/src/main/kotlin/it/dogior/hadEnough/AnimeSaturnPlugin.kt
+++ AnimeSaturn/src/main/kotlin/it/dogior/hadEnough/AnimeSaturnPlugin.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import com.lagradost.cloudstream3.plugins.Plugin
 import com.lagradost.cloudstream3.plugins.PluginManager
 
-class PluginAnimeSaturn : Plugin() {
+class AnimeSaturnPlugin : Plugin() {
     override fun load(context: Context) {
         PluginManager.registerMainAPI(AnimeSaturn())
     }
