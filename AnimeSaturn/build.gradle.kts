635ebd39ae4162c3b0540ed9d2f526852af65f7f
brusus <rafael.fiallo93@gmail.com>
2025-08-27 07:34:36 +0200

    Update build.gradle.kts

--- AnimeSaturn/build.gradle.kts
+++ AnimeSaturn/build.gradle.kts
@@ -1,29 +1,20 @@
-// use an integer for version numbers
-version = 1
-
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("com.lagradost.cloudstream3.gradle")
+}
 
 cloudstream {
-    // All of these properties are optional, you can safely remove them
-
-    description = "Animesaturn"
-    authors = listOf("doGior","DieGon")
-
-    /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
-    status = 1
-    tvTypes = listOf(
-        "AnimeMovie",
-        "Anime",
-        "OVA",
-    )
-
     language = "it"
-    requiresResources = false
-
+    description = "Anime da AnimeSaturn (SUB ITA & ITA)."
+    authors = listOf("brusus")
     iconUrl = "https://www.animesaturn.cx/immagini/favicon-32x32.png"
+    tvTypes = listOf("Anime")
+}
+
+android {
+    // Namespace UNICO per il modulo, ma può differire dal package Kotlin
+    namespace = "it.dogior.hadEnough.animesaturn"
+    compileSdk = 34
+    defaultConfig { minSdk = 21 }
 }
