0ab2189aa1e5fc49fd84ded6e5b4764012bd0d70
brusus <rafael.fiallo93@gmail.com>
2025-08-27 08:39:34 +0200

    Update StreamingCommunity.kt

--- StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunity.kt
+++ StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunity.kt
@@ -46,7 +46,7 @@ class StreamingCommunity : MainAPI() {
             "X-Inertia-Version" to inertiaVersion,
             "X-Requested-With" to "XMLHttpRequest",
         ).toMutableMap()
-        val mainUrl = "https://streamingcommunityz.chat"
+        val mainUrl = "https://streamingcommunityz/it"
         var name = "StreamingCommunity"
     }
 
